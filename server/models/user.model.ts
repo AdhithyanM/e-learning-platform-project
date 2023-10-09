5e085793c468f06aa0c7ce96bf57fd413d424c01
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-10-09 13:25:34 +0530

    login and logout functionality with redis for session management

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,5 +1,7 @@
+require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,6 +17,8 @@ export interface IUser extends Document {
   isVerified: boolean;
   courses: Array<{ courseId: string }>;
   comparePassword: (password: string) => Promise<boolean>;
+  signAccessToken: () => string;
+  signRefreshToken: () => string;
 }
 
 const userSchema: Schema<IUser> = new Schema(
@@ -74,6 +78,16 @@ userSchema.methods.comparePassword = async function (
   enteredPassword: string
 ): Promise<boolean> {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// sign access token
+userSchema.methods.signAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN_SECRET || "");
+};
+
+// sign refresh token
+userSchema.methods.signRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET || "");
 };
 
 const userModel: Model<IUser> = mongoose.model("User", userSchema);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { registerUser, activateUser } from "../controllers/user.controller";
+import {
+  registerUser,
+  activateUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/registerUser", registerUser);
 
 userRouter.post("/activateUser", activateUser);
+
+userRouter.post("/login", loginUser);
+
+userRouter.post("/logout", logoutUser);
 
 export default userRouter;
